ca326060a85c14d4a7ad05a2f4e7e46a79728d63
khushaalan <khushaalanarjunan@gmail.com>
2023-06-13 19:07:49 +0800

    'update'

--- LAB02-TASK01.c
+++ LAB02-TASK01.c
@@ -15,6 +15,8 @@ int calculateSum(int arr[], int size){
     return sum;
 }
 
+
+
 struct Max findMaximum(int arr[], int size){
     struct Max max;
     max.max = arr[0],max.index = 0;
@@ -27,14 +29,19 @@ struct Max findMaximum(int arr[], int size){
     return max;
 }
 
-void reverseArray(int arr[], int size){
-    int temp;
-    for(int i=0;i<size/2;i++){
-        temp = arr[i];
-        arr[i] = arr[size-i-1];
-        arr[size-i-1] = temp;
+void reverseArray(int *arr, int size){
+    int *pos = arr;
+    int *end = arr + size - 1;
+    while (pos < end){
+        int temp = *pos;
+        *pos = *end;
+        *end = temp;
+        pos++;
+        end--;
     }
 }
+
+
 
 int main(){
     srand(time(0));
@@ -52,11 +59,15 @@ int main(){
 
     printf("\nSum of all elements: %d",calculateSum(arr,10));
     printf("\nMaximum element: %d, Index: %d",findMaximum(arr,10).max, findMaximum(arr,10).index);
+
+    
     reverseArray(arr,10);
     printf("\nReversed array : ");
     for(int i=0;i<10;i++){
         printf("%d ",arr[i]);
     }
+
+
     
     return 0;
 }
