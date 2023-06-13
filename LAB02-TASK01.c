70a84a1b3af00ff116ce78d428691d0b9155b288
khushaalan <khushaalanarjunan@gmail.com>
2023-06-13 14:49:17 +0800

    'update'

---
+++ LAB02-TASK01.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include <stdlib.h>  
+#include <time.h>
+
+struct Max{
+    int max;
+    int index;
+};
+
+int calculateSum(int arr[], int size){
+    int sum = 0;
+    for(int i=0;i<size;i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+struct Max findMaximum(int arr[], int size){
+    struct Max max;
+    max.max = arr[0],max.index = 0;
+    for(int i=1;i<size;i++){
+        if(arr[i] > max.max){
+            max.max = arr[i];
+            max.index = i;
+        }
+    }
+    return max;
+}
+
+void reverseArray(int arr[], int size){
+    int temp;
+    for(int i=0;i<size/2;i++){
+        temp = arr[i];
+        arr[i] = arr[size-i-1];
+        arr[size-i-1] = temp;
+    }
+}
+
+int main(){
+    srand(time(0));
+    
+    int arr[10];
+    for(int i=0;i<10;i++){
+        int num = rand() % 100 + 1;
+        arr[i] = num;
+    }
+
+    printf("Original Array: ");
+    for(int i=0;i<10;i++){
+        printf("%d ",arr[i]);
+    }
+
+    printf("\nSum of all elements: %d",calculateSum(arr,10));
+    printf("\nMaximum element: %d, Index: %d",findMaximum(arr,10).max, findMaximum(arr,10).index);
+    reverseArray(arr,10);
+    printf("\nReversed array : ");
+    for(int i=0;i<10;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
---
+++ MAKEFILE.py
@@ -0,0 +1,49 @@
+# pylint: disable=all
+
+
+import os,sys
+WARNING = "-Wall -Wextra"
+COMMAND =""
+
+def compileTask1():
+    COMMAND = "gcc "+ WARNING + " LAB02-TASK01.c -o LAB02-TASK01.exe && LAB02-TASK01.exe"
+    os.system(COMMAND)
+    if os.path.exists("LAB02-TASK01.exe"):
+        os.remove("LAB02-TASK01.exe")
+
+def compileTask2():
+    COMMAND = "gcc "+ WARNING + " LAB02-TASK02.c -o LAB02-TASK02.exe && LAB02-TASK02.exe"
+    os.system(COMMAND)
+    if os.path.exists("LAB02-TASK02.exe"):
+        os.remove("LAB02-TASK02.exe")
+
+
+## https://github.com/khushaalan/Imperative-Programming.git
+def gitUpdate():
+    os.system("git status")
+    os.system("git add -A")
+    os.system("git commit -m 'update'")
+    os.system("git push origin main")
+    os.system("git status")
+
+
+def main(args=None):
+    args = args or sys.argv[1:]
+    if args:
+        if args[0] == '--1':
+            compileTask1()
+        elif args[0] == '--2':
+            compileTask2()
+        elif args[0] == '--git':
+            gitUpdate()
+        else:
+            print("Usage: python MAKEFILE.py [ --1 | --2 | --git ]")
+            print("Example: python MAKEFILE.py --1")
+            return
+    else:
+        print("Usage: python MAKEFILE.py [ --1 | --2 | --git ]")
+        print("Example: python MAKEFILE.py --1")
+        return
+
+
+main()
